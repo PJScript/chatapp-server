8e34f98657023e65a053e4086dd4b4fec5f2106f
PJScript <kilo718@naver.com>
2022-05-31 08:41:31 +0900

    fix limit

--- app.js
+++ app.js
@@ -157,7 +157,7 @@ app.get('/logout', (req, res) => {
 })
 
 app.post('/clearall', (req,res) => {
-    connection.query(`select * from chats order by id desc LIMIT 1,1;`, (err, result) => {
+    connection.query(`select * from chats order by id desc LIMIT 0,1;`, (err, result) => {
         console.log(result[0].id, "result chats")
         let idx = result[0].id + 1
         console.log( typeof(result[0].id) )
